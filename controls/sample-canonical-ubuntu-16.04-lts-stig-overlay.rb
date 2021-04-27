67aa193916dd44aa79e156f9aed50559899d1816
Nick Johnson <22401371+njohnson11@users.noreply.github.com>
2021-04-27 10:12:01 -0400

    Adding controls to fix some profile errors occurring when auditd and ufw are not installed or if a user does not have a shell specified

--- controls/sample-canonical-ubuntu-16.04-lts-stig-overlay.rb
+++ controls/sample-canonical-ubuntu-16.04-lts-stig-overlay.rb
@@ -1,6 +1,239 @@
 
 include_controls 'canonical-ubuntu-16.04-lts-stig-baseline' do
-
+    control 'V-75461' do
+        non_interactive_shells = input('non_interactive_shells')
+        ignore_shells = non_interactive_shells.join('|')
+        counter = 0
+    
+        users.where { shell != nil && !shell.match(ignore_shells) }.entries.each do |user_info|
+            shadow.where(user: user_info.username).passwords.each do |user_pwd|
+                pwd_should_be_evaluated = !(user_pwd.casecmp?('!') || user_pwd.casecmp?('*'))
+                next unless pwd_should_be_evaluated
+    
+                describe (user_info.username + ' - user\'s password hash') do
+                    subject { user_pwd }
+                    it { should start_with '$6'} 
+                end
+                counter += 1
+            end
+        end
+        if counter == 0
+            describe 'Number of interactive users on the system' do
+            subject { counter }
+                it { should be 0 }
+            end
+        end
+    end
+    control 'V-75559' do
+        exempt_home_users = input('exempt_home_users')
+        non_interactive_shells = input('non_interactive_shells')
+        ignore_shells = non_interactive_shells.join('|')
+    
+        users.where { (shell != nil && !shell.match(ignore_shells)) && (uid >= 1000 || uid == 0) }.entries.each do |user_info|
+            next if exempt_home_users.include?(user_info.username.to_s)
+    
+            describe directory(user_info.home) do
+                it { should exist }
+            end
+        end
+    end
+    control 'V-75563' do
+        exempt_home_users = input('exempt_home_users')
+        non_interactive_shells = input('non_interactive_shells')
+        ignore_shells = non_interactive_shells.join('|')
+    
+        users.where { (shell != nil && !shell.match(ignore_shells)) && (uid >= 1000 || uid == 0) }.entries.each do |user_info|
+            next if exempt_home_users.include?(user_info.username.to_s)
+    
+            describe directory(user_info.home) do
+                it { should exist }
+            end
+        end
+    end
+    control 'V-75565' do
+        exempt_home_users = input('exempt_home_users')
+        non_interactive_shells = input('non_interactive_shells')
+        ignore_shells = non_interactive_shells.join('|')
+    
+        findings = Set[]
+        users.where { (shell != nil && !shell.match(ignore_shells)) && (uid >= 1000 || uid == 0) }.entries.each do |user_info|
+            next if exempt_home_users.include?(user_info.username.to_s)
+    
+            findings += command("find #{user_info.home} -maxdepth 0 -perm /027").stdout.split("\n")
+        end
+        describe 'Home directories with excessive permissions' do
+            subject { findings.to_a }
+            it { should be_empty }
+        end
+    end
+    control 'V-75567' do
+        exempt_home_users = input('exempt_home_users')
+        non_interactive_shells = input('non_interactive_shells')
+        ignore_shells = non_interactive_shells.join('|')
+      
+        findings = Set[]
+        users.where { (shell != nil && !shell.match(ignore_shells)) && (uid >= 1000 || uid == 0) }.entries.each do |user_info|
+          next if exempt_home_users.include?(user_info.username.to_s)
+      
+          findings += command("find #{user_info.home} -maxdepth 0 -not -gid #{user_info.gid}").stdout.split("\n")
+        end
+        describe "Home directories that are not group-owned by the user's primary GID" do
+          subject { findings.to_a }
+          it { should be_empty }
+        end
+    end
+    control 'V-75569' do
+        non_interactive_shells = input('non_interactive_shells')
+        ignore_shells = non_interactive_shells.join('|')
+    
+        findings = Set[]
+        users.where { (shell != nil && !shell.match(ignore_shells)) && (uid >= 1000 || uid == 0) }.entries.each do |user_info|
+            dot_files = command("find #{user_info.home} -xdev -maxdepth 1 -name '.*' -type f").stdout.split("\n")
+            dot_files.each do |dot_file|
+            next unless file(dot_file).more_permissive_than?('0740')
+    
+            findings << dot_file
+            end
+        end
+        describe 'All local initialization files have a mode of 0740 or less permissive' do
+            subject { findings.to_a }
+            it { should be_empty }
+        end
+    end
+    control 'V-75571' do
+        exempt_home_users = input('exempt_home_users')
+        non_interactive_shells = input('non_interactive_shells')
+        ignore_shells = non_interactive_shells.join('|')
+    
+        findings = Set[]
+        users.where { (shell != nil && !shell.match(ignore_shells)) && (uid >= 1000 || uid == 0) }.entries.each do |user_info|
+            next if exempt_home_users.include?(user_info.username.to_s)
+    
+            grep_results = command("grep -i path --exclude=\".bash_history\" #{user_info.home}/.*").stdout.split('\\n')
+            grep_results.each do |result|
+            result.slice! 'PATH='
+            result += ' ' if result[-1] == ':'
+            result.slice! '$PATH:'
+            result.slice! "$PATH\"\n"
+            result.gsub! '$HOME', user_info.home.to_s
+            result.gsub! '~', user_info.home.to_s
+            line_arr = result.split(':')
+            line_arr.delete_at(0)
+            line_arr.each do |line|
+                line.slice! '"'
+                next unless !line.start_with?('export') && !line.start_with?('#')
+    
+                if line.strip.empty?
+                curr_work_dir = command('pwd').stdout.gsub("\n", '')
+                line = curr_work_dir if curr_work_dir.start_with?(user_info.home.to_s)
+                end
+                findings.add(line) unless line.start_with?(user_info.home)
+            end
+            end
+        end
+        describe 'Initialization files that include executable search paths that include directories outside their home directories' do
+            subject { findings.to_a }
+            it { should be_empty }
+        end
+    end
+    control 'V-75573' do
+        disable_slow_controls = input('disable_slow_controls')
+        non_interactive_shells = input('non_interactive_shells')
+        if disable_slow_controls
+            describe 'This control consistently takes a long to run and has been disabled using the DISABLE_SLOW_CONTROLS attribute.' do
+            skip "This control consistently takes a long to run and has been disabled
+            using the DISABLE_SLOW_CONTROLS attribute. You must enable this control for a
+            full accredidation for production."
+            end
+        else
+            ignore_shells = non_interactive_shells.join('|')
+    
+            dotfiles = Set[]
+            u = users.where { (shell != nil && !shell.match(ignore_shells)) && (uid >= 1000 || uid == 0) }.entries
+                u.each do |user|
+                dotfiles += command("find #{user.home} -xdev -maxdepth 2 -name '.*' ! -name \".bash_history\" -type f").stdout.split("\n")
+            end
+            ww_files = Set[]
+            ww_files = command('find / -perm -002 -type f -exec ls {} \;').stdout.lines
+            findings = Set[]
+            dotfiles.each do |dotfile|
+                dotfile = dotfile.strip
+                ww_files.each do |ww_file|
+                    ww_file = ww_file.strip
+                    count = command("grep -c \"#{ww_file}\" \"#{dotfile}\"").stdout.strip.to_i
+                    findings << dotfile if count > 0
+                end
+            end
+            describe 'Local initialization files that are found to reference world-writable files' do
+                subject { findings.to_a }
+                it { should be_empty }
+            end
+        end
+    end
+    control 'V-75587' do
+        non_interactive_shells = input('non_interactive_shells')
+        exempt_home_users = input('exempt_home_users')
+        ignore_shells = non_interactive_shells.join('|')
+    
+        users.where { (shell != nil && !shell.match(ignore_shells)) && (uid >= 1000) }.entries.each do |user_info|
+            next if exempt_home_users.include?(user_info.username.to_s)
+    
+            home_mount = command(%(df #{user_info.home} --output=target | tail -1)).stdout.strip
+            describe user_info.username do
+                context 'with mountpoint' do
+                    context home_mount do
+                    it { should_not be_empty }
+                    it { should_not match(%r{^/$}) }
+                    end
+                end
+            end
+        end
+    end
+    # Unsure as to whether a profile error is not an expected result here
+    control 'V-75689' do
+        describe package('auditd') do
+            it { should be_installed }
+        end
+        if package('auditd').installed?
+            if os.arch == 'x86_64'
+                describe auditd.syscall('execve').where { arch == 'b64' } do
+                    its('action.uniq') { should eq ['always'] }
+                    its('list.uniq') { should eq ['exit'] }
+                end
+            end
+            describe auditd.syscall('execve').where { arch == 'b32' } do
+                its('action.uniq') { should eq ['always'] }
+                its('list.uniq') { should eq ['exit'] }
+            end
+        end
+    end
+    control 'V-75809' do
+        ufw_status = command('ufw status').stdout.strip.lines.first
+        value = ufw_status != nil ? ufw_status.split(':')[1].strip : 'not installed'
+    
+        describe 'UFW status' do
+            subject { value }
+            it { should cmp 'active' }
+        end
+        describe 'Status listings for any allowed services, ports, or applications must be documented with the organization' do
+            skip 'Status listings checks must be preformed manually'
+        end
+    end
+    control 'V-75855' do
+        ufw_status_output = command('ufw status').stdout.strip
+        is_ufw_active = ufw_status_output != "" ? !ufw_status_output.lines.first.include?('inactive') : false
+    
+        if is_ufw_active
+            describe ufw_status_output do
+              it { should match /(LIMIT)/ }
+            end
+        else
+            describe 'UFW status is active' do
+              subject { is_ufw_active }
+              it { should be true }
+            end
+        end
+    end
     control 'V-75389' do
         impact 0.0
         desc 'caveat', 'This is Not Applicable since the related security control is not included in ***SPONSOR*** policy'
